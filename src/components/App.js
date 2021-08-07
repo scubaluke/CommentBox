94418ec9001a891a76d50361abf70393c7fd1a1e
Luke Gartland <lukegartland@gmail.com>
2021-08-06 17:25:05 -1000

    setting up project and first test

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CommentList from './CommentList'
+import CommentBox from './CommentBox'
+
+export default function App() {
+    return (
+        <div>
+            <CommentBox />
+            <CommentList />
+        </div>
+    )
+}
---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+it('Shows a comment box' , () => {
+    const div = document.createElement('div')
+
+    ReactDOM.render(<App />, div);
+
+    ReactDOM.unmountComponentAtNode(div)
+})
