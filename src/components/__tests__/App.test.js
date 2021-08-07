70240fa16475dd6ffa2ef5da74ad54a898533610
Luke Gartland <lukegartland@gmail.com>
2021-08-07 08:55:25 -1000

    added Comment List component test

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,9 +2,16 @@ import React from 'react';
 import App from '../App';
 import { shallow } from 'enzyme'
 import CommentBox from '../CommentBox'
+import CommentList from '../CommentList'
 
-it('renders a comment box' , () => {
+it('renders a Comment Box component' , () => {
   const wrapped = shallow(<App />);
 
     expect(wrapped.find(CommentBox).length).toEqual(1)
 })
+
+it('renders a Comment List component', () => {
+    const wrapped = shallow(<App />);
+
+    expect(wrapped.find(CommentList).length).toEqual(1)
+}) 
