591313c69092ae10cf18bf0ea75d84fa00dd2c67
Luke Gartland <lukegartland@gmail.com>
2021-08-08 08:23:25 -1000

    refactor with describe

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,25 +17,24 @@ it('has a text area and a button', () => {
     expect(wrapped.find('button').length).toEqual(1)
 })
 
-it('has a text area that users can type in', () => {
-    wrapped.find('textarea').simulate('change', {
-        target: { value: 'new comment' }
+describe('the text area',  () => {
+    beforeEach(() => {
+         wrapped.find('textarea').simulate('change', {
+                target: { value: 'new comment' }
+            })
+            wrapped.update();
     })
-    wrapped.update();
 
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+    it('has a text area that users can type in', () => {
+        expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+    })
+
+    // create submit event on form
+    it('submits form and clears text area', () => {
+        wrapped.find('form').simulate('submit')
+        wrapped.update()
+        expect(wrapped.find('textarea').prop('value')).toEqual('')
+    })
 })
 
-// create submit event on form
-it('submits form and clears text area', () => {
-    wrapped.find('textarea').simulate('change', {
-        target: { value: 'new comment' }
-    })
-    wrapped.update();
-    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
-
-    wrapped.find('form').simulate('submit')
-    wrapped.update()
-    expect(wrapped.find('textarea').prop('value')).toEqual('')
-})
 
