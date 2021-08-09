7522872d243cb80c60f1c62d9ddf9c99cc0d10af
Luke Gartland <lukegartland@gmail.com>
2021-08-09 08:44:22 -1000

    testing action

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -9,3 +9,8 @@ it('handles actions of type SAVE_COMMENT', () => {
     const newState = commentsReducer([], action)
     expect(newState).toEqual(['New Comment'])
 }) 
+
+it('handles action with unknown type', () => {
+    const newState = commentsReducer([], {})
+    expect(newState).toEqual([])
+})
